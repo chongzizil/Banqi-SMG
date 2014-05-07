840ff0682ad69da3f9ce0849fd0bca48e3b74b67
chongzizil <chongzizil@gmail.com>
2014-05-07 03:22:11 -0400

    HW10: Fixed animation problem (miracle...)
    1. Previously when the player makes the move, the state won't be updated
    until the ai makes the move... so the state updates two times at
    once.... Now I added a timer to delay the ai and it worked... Though
    there's still no animation for the AI. But everything is better now...
    
    2. AI response time is increased to 1.75s (Since there's 1s animation,
    so in my opinion it's not too long...) And the AI is good :) Though luck
    is the most import thing in this game, but the AI will always makes a
    good move.

--- src/org/banqi/client/BanqiPresenter.java
+++ src/org/banqi/client/BanqiPresenter.java
@@ -14,6 +14,7 @@ import org.game_api.GameApi.UpdateUI;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.gwt.user.client.Timer;
 
 /**
  * The presenter that controls the banqi graphics. We use the MVP pattern: the
@@ -159,40 +160,48 @@ public class BanqiPresenter {
           // The game is over, send the endGame operation
           endGame();
         } else {
-          // The game is not over, make the move :)
-          Heuristic heuristic = new Heuristic();
-          AlphaBetaPruning ai = new AlphaBetaPruning(heuristic, banqiState);
           
-          // The move of the AI takes at most 0.8 second
-          DateTimer timer = new DateTimer(2000);
-          
-          // The depth is 50 though due to the time limit, it may not reach that deep
-          Move move = ai.findBestMove(50, timer);
-          
-          // AI make the move.
-          int selectedFromCoord = ((move.getFrom().getRow() - 1) * 8 + move.getFrom().getCol()) - 1;
-          int selectedToCoord = ((move.getTo().getRow() - 1) * 8 + move.getTo().getCol()) - 1;
-          if (move.getType() == Move.Type.CAPTURE) {
-            
-            Set capturePiece = new Set(CAPTUREPIECE, ImmutableList.of("C"
-                + selectedFromCoord, "C" + selectedToCoord));
-            view.setAnimateArgs(banqiState.getCells(), selectedFromCoord, selectedToCoord,
-                true, false);
-            capturePiece(capturePiece);
-          } else if (move.getType() == Move.Type.MOVE) {
-            
-            Set movePiece = new Set(MOVEPIECE, ImmutableList.of("C"
-                + selectedFromCoord, "C" + selectedToCoord));
-            view.setAnimateArgs(banqiState.getCells(), selectedFromCoord, selectedToCoord,
-                false, false);
-            movePiece(movePiece);
-          } else {
-            
-            Set turnPiece = new Set(TURNPIECE, "C" + selectedFromCoord);
-            view.setAnimateArgs(banqiState.getCells(), selectedFromCoord, selectedFromCoord,
-                false, false);
-            turnPiece(turnPiece);
-          }
+          Timer aiTimer = new Timer() {
+            public void run() {
+              // The game is not over, make the move :)
+              Heuristic heuristic = new Heuristic();
+              AlphaBetaPruning ai = new AlphaBetaPruning(heuristic, banqiState);
+              
+              // The move of the AI takes at most 1.75 second
+              DateTimer timer = new DateTimer(1750);
+              
+              // The depth is 50 though due to the time limit, it may not reach that deep
+              Move move = ai.findBestMove(50, timer);
+              
+              // AI make the move.
+              int selectedFromCoord = ((move.getFrom().getRow() - 1) * 8
+                  + move.getFrom().getCol()) - 1;
+              int selectedToCoord = ((move.getTo().getRow() - 1) * 8
+                  + move.getTo().getCol()) - 1;
+              if (move.getType() == Move.Type.CAPTURE) {
+                
+                Set capturePiece = new Set(CAPTUREPIECE, ImmutableList.of("C"
+                    + selectedFromCoord, "C" + selectedToCoord));
+                view.setAnimateArgs(banqiState.getCells(), selectedFromCoord, selectedToCoord,
+                    true, false);
+                capturePiece(capturePiece);
+              } else if (move.getType() == Move.Type.MOVE) {
+                
+                Set movePiece = new Set(MOVEPIECE, ImmutableList.of("C"
+                    + selectedFromCoord, "C" + selectedToCoord));
+                view.setAnimateArgs(banqiState.getCells(), selectedFromCoord, selectedToCoord,
+                    false, false);
+                movePiece(movePiece);
+              } else {
+                
+                Set turnPiece = new Set(TURNPIECE, "C" + selectedFromCoord);
+                view.setAnimateArgs(banqiState.getCells(), selectedFromCoord, selectedFromCoord,
+                    false, false);
+                turnPiece(turnPiece);
+              }
+            }
+          };
+          aiTimer.schedule(800);
         }
       } 
       return;
